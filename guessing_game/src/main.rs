f827a8dc87f4397683cf2bdd1e51bdf0a121cf38
Aeshma <madhushanka2727@gmail.com>
2025-02-17 18:12:05 +0530

    add a loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number...");
-    println!("Please enter your guess");
     
-    let mut guess = String::new();
+    let secret_number = rand::rng().random_range(1..=100);
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the line");
+    println!("The secret number is: {secret_number}");
     
-    println!("Your guessed: {}", guess);
+    loop {
+        
+        println!("Please enter your guess");
+        
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("Your guessed: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break; 
+            }
+        }
+    }
 }
